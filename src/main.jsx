def1736cacfede084e9f5af643060f0b88821a72
GeonJoseph <159272754+GeonJoseph@users.noreply.github.com>
2026-02-17 22:50:12 +0530

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Import the Router
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* The basename tells React Router the app is in a subfolder */}
+    <BrowserRouter basename="/career-navigator">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
